4af89217d410618d2c87e2f174f19a96df8d28f8
grigor-aramyan <grigori.aramyan@gmail.com>
2018-04-24 00:09:21 +0400

    initial commit

---
+++ app/src/main/java/com/mycompany/john/pickaplace/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mycompany.john.pickaplace.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mycompany.john.pickaplace.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
