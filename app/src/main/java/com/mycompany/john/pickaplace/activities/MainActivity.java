d8a7e272029027bf2c7cc953bceef01cd55b29d0
grigor-aramyan <grigori.aramyan@gmail.com>
2018-04-24 21:38:41 +0400

    add login and check the code dialogs

--- app/src/main/java/com/mycompany/john/pickaplace/activities/MainActivity.java
+++ app/src/main/java/com/mycompany/john/pickaplace/activities/MainActivity.java
@@ -1,15 +1,131 @@
 package com.mycompany.john.pickaplace.activities;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.mycompany.john.pickaplace.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private View.OnClickListener mClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.login_link:
+                    prepareLoginDialog();
+                    Toast.makeText(getApplicationContext(), "login...", Toast.LENGTH_LONG).show();
+                    break;
+                case R.id.register_link:
+                    startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+                    break;
+                case R.id.pick_btn_id:
+                    Toast.makeText(getApplicationContext(), "pick...", Toast.LENGTH_LONG).show();
+                    break;
+                case R.id.code_btn_id:
+                    prepareEnterCodeDialog();
+                    Toast.makeText(getApplicationContext(), "code...", Toast.LENGTH_LONG).show();
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    // UI components
+    private TextView mLoginTxt, mRegisterTxt;
+    private Button mPickAPlaceBtn, mEnterCodeBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void prepareLoginDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.layout_login_dialog, null);
+        final EditText emailEdt = (EditText) view.findViewById(R.id.email_edt_id);
+        final EditText passwordEdt = (EditText) view.findViewById(R.id.password_edt_id);
+
+        builder.setTitle("Login")
+                .setView(view)
+                .setMessage("Email/Password, please ))")
+                .setPositiveButton("Login", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String enteredEmail = emailEdt.getText().toString();
+                        String enteredPassword = passwordEdt.getText().toString();
+                        if (enteredEmail.isEmpty() || enteredPassword.isEmpty()) {
+                            Toast.makeText(getApplicationContext(), "Both fields are required!",
+                                    Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "checking...", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        builder.setCancelable(true);
+        builder.create().show();
+    }
+
+    private void prepareEnterCodeDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.layout_enter_shared_code_dialog, null);
+        final EditText codeEdt = (EditText) view.findViewById(R.id.code_edt_id);
+
+        builder.setTitle("Enter the Code")
+                .setView(view)
+                .setMessage("Shared code for picked location")
+                .setPositiveButton("Check", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String enteredCode = codeEdt.getText().toString();
+                        if (enteredCode.isEmpty()) {
+                            Toast.makeText(getApplicationContext(), "Enter some code " +
+                            "for me to check :)", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "checking...", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        builder.setCancelable(true);
+        builder.create().show();
+    }
+
+    private void initViews() {
+        mLoginTxt = (TextView) findViewById(R.id.login_link);
+        mLoginTxt.setOnClickListener(mClickListener);
+        mRegisterTxt = (TextView) findViewById(R.id.register_link);
+        mRegisterTxt.setOnClickListener(mClickListener);
+        mPickAPlaceBtn = (Button) findViewById(R.id.pick_btn_id);
+        mPickAPlaceBtn.setOnClickListener(mClickListener);
+        mEnterCodeBtn = (Button) findViewById(R.id.code_btn_id);
+        mEnterCodeBtn.setOnClickListener(mClickListener);
     }
 }
---
+++ app/src/main/java/com/mycompany/john/pickaplace/activities/RegisterActivity.java
@@ -0,0 +1,32 @@
+package com.mycompany.john.pickaplace.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.mycompany.john.pickaplace.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
